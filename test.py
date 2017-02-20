000344641dacb172197ad125419f0b0006b9b62f
wangminghui <wangminghui@qiyi.com>
2017-02-20 16:57:57 +0800

    Const file for python
    Signed-off-by: wangminghui <wangminghui@qiyi.com>

---
+++ test.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+import const
+
+
+if __name__ == "__main__":
+    const.COM = "IBM"
+    print const.COM
+    const.COM = "test"
+    print const.COM
+    const.name = "iqiyi"
+    print const.name
